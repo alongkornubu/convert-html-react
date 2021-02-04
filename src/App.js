569f8af390749ec06586c07b281af10b25ab9c17
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2021-02-04 21:51:18 +0700

    2021-02-04

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './Header'
+import Portfolio from './Portfolio'
+import Aboutsection from './Aboutsection'
+import Contact from './Contact'
+import Footer from './Footer'
+import Allportfolio from './Allportfolio'
+function App() {
+  return (
+    <>
+      <Header/>
+      <Portfolio/>
+      <Aboutsection/>
+      <Contact/>
+      <Footer/>
+      <Allportfolio/>
+    </>
+  );
+}
+
+export default App;
